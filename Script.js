c331cf0b27288ec6ae894ff036371463e9e6dfdd
Mahadgaming5349 <162704755+MAR-Official-YT@users.noreply.github.com>
2025-01-31 23:53:01 +0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,21 +1,57 @@
 let display = document.getElementById('display');
+let currentInput = '';
+let currentOperation = null;
+let previousNumber = null;
 
 function appendValue(value) {
-    display.value += value;
+    currentInput += value;
+    display.value = currentInput;
 }
 
 function clearDisplay() {
+    currentInput = '';
+    currentOperation = null;
+    previousNumber = null;
     display.value = '';
 }
 
 function backspace() {
-    display.value = display.value.slice(0, -1);
+  currentInput = currentInput.slice(0, -1);
+    display.value = currentInput;
 }
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
+    if (currentInput === '') return;
+  let newNumber = Number(currentInput)
+
+    if (previousNumber === null) {
+      previousNumber = newNumber;
+      currentInput = '';
+      return;
     }
+
+    let result = 0;
+
+    switch (currentOperation) {
+        case '+':
+          result = previousNumber + newNumber;
+          break;
+      case '-':
+          result = previousNumber - newNumber;
+          break;
+      case '*':
+          result = previousNumber * newNumber;
+        break;
+      case '/':
+          result = previousNumber / newNumber;
+        break;
+      default:
+          result = newNumber;
+          break;
+    }
+
+    previousNumber = result;
+    currentInput = '';
+    display.value = result;
+
 }
